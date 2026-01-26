a7194f167ef970b2eed5d330c50d86c9bbf4773c
ReggeMeister <mattiaswaern04@gmail.com>
2026-01-26 10:22:00 +0100

    commit

--- milkshake/src/components/views/MilkShakeMap.jsx
+++ milkshake/src/components/views/MilkShakeMap.jsx
@@ -4,32 +4,42 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
+
+const milkshakePlaces = [
+    {name: "DirtyCoco", coords: [59.3293, 18.0686] },
+];
+
 export default function MilkshakeMap() {
     const mapContainer = useRef(null);
     const mapRef = useRef(null);
-    const [places, setPlaces] = useState([]);
-
 
     useEffect(() => {
-        fetch("http://localhost:3000/api/places")
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/stockholm.json?access_token=${import.meta.env.VITE_MAPBOX_TOKEN}`)
         .then(res => res.json())
-        .then(data => setPlaces(data))
+        .then(data => console.log("MapBox API fetched:", data))
         .catch(err => console.error(err));
     }, []);
 
+ 
     useEffect(() => {
-        if (!mapRef.current || places.length === 0) return;
+        if (mapRef.current) return;
 
-        places.forEach(place => {
-            const popup = new mapboxgl.Popup({offset: 25})
-            .setText(place.name);
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [18.0686, 59.3293],
+            zoom: 13
+        });
 
+        milkshakePlaces.forEach(place => {
             new mapboxgl.Marker()
-            .setLngLat([place.lng, place.lat])
-            .setPopup(popup)
+            .setLngLat(place.coords)
+            .setPopup(new mapboxgl.Popup().setText(place.name))
             .addTo(mapRef.current);
         });
-    }, [places]);
+
+        return () => mapRef.current?.remove();
+    }, []);
 
 
     return (
